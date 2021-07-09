7308732e8b50aea025ceaf822935e81c7b10b1e1
JackPeng123 <Jack.PENG@REInventVentures.com>
2021-07-09 18:21:32 +0800

    增加RPC框架-初版

---
+++ src/main/java/com/example/java_practice/rpc/provider/RpcProvider.java
@@ -0,0 +1,27 @@
+package com.example.java_practice.rpc.provider;
+
+public interface RpcProvider {
+
+    /**
+     * 客户端获取远程服务的代理实现Stub
+     *
+     * @param clazz
+     * @param <T>
+     */
+   <T> T getRemoteService(Class<T> clazz);
+
+
+    /**
+     * 服务端注册服务可以给客户端使用
+     *
+     * @param t
+     * @param <T>
+     * @return
+     */
+   <T> ServerUrl registerService(T t);
+
+    /**
+     * 启动RPC服务
+     */
+   void startService();
+}
---
+++ src/main/java/com/example/java_practice/rpc/provider/ServerUrl.java
@@ -0,0 +1,13 @@
+package com.example.java_practice.rpc.provider;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class ServerUrl {
+    private String name;
+    private String host;
+    private Integer port;
+}
